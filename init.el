e9cdddfd7803feac0100dc6cb243ed856c1abf88
InventorXtreme <inventorxtreme@gmail.com>
2023-11-13 20:20:44 -0500

    upload

---
+++ init.el
@@ -0,0 +1,80 @@
+(setq scheme-program-name "guile")
+(load-theme 'wombat t)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  (add-to-list 'load-path "<path where use-package is installed>")
+  (require 'use-package))
+
+
+(use-package vterm
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+
+
+;; provides autocomplete and built in errors
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (XXX-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp
+  :ensure t
+  )
+
+
+;; base ui elements for lsp
+(use-package lsp-ui :commands lsp-ui-mode
+  :ensure t
+  )
+
+(use-package company
+  :ensure t
+ ) 
+(use-package go-mode
+  :ensure t
+  )
+
+
+;;sycs emacs and system path
+(use-package exec-path-from-shell
+  :ensure t
+  )
+
+
+(cua-mode)
+
+(package-initialize)
+
+(setq company-idle-delay 0)
+
+(exec-path-from-shell-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(lsp-enable-symbol-highlighting t)
+ '(package-selected-packages '(which-key use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
