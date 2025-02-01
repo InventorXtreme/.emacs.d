164f9fb648fe7e760779ce337b3dbf21f96421e2
Inventor Xtreme <inventorxtreme@gmail.com>
2025-01-31 22:30:15 -0500

    message when gcing

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 
 (setq gc-cons-threshold (eval-when-compile (* 1024 1024 1024)))
 (run-with-idle-timer 2 t (lambda () (garbage-collect)))
-
+(setq garbage-collection-messages t)
 
 (setq scheme-program-name "guile")
 (load-theme 'wombat t)
